fdcd7330d20731bd712a24b8275b9e2f9858ae75
mrjambert <79665732+mrjambert@users.noreply.github.com>
2022-03-20 18:56:39 -0400

    Update main.c

--- Exercises/MapReduce/main.c
+++ Exercises/MapReduce/main.c
@@ -161,7 +161,7 @@ int main()
 
     /* Read and print the result */
     ret = clEnqueueReadBuffer(command_queue, result_buffer, CL_TRUE, 0,
-       sizeof(result), &result, 0, NULL, NULL);
+       sizeof(result), result, 0, NULL, NULL);
     if(ret < 0) {
        perror("Couldn't read the buffer");
        exit(1);
